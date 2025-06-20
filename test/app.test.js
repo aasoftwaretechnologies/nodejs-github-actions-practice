fd5e1d583a96e741582f5804e47b187f74c73264
aasoftwaretechnologies <aasoftwaretechnologies@gmail.com>
2025-06-20 16:03:46 +0530

    Update app.test.js

--- test/app.test.js
+++ test/app.test.js
@@ -1,1 +1,11 @@
+const request = require('supertest');
+const app = require('../src/app');
+
+describe('GET /', () => {
+  it('should return Hello GitHub Actions!', async () => {
+    const res = await request(app).get('/');
+    expect(res.statusCode).toEqual(200);
+    expect(res.text).toBe('Hello GitHub Actions!');
+  });
+});
 
