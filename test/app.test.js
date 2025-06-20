dc5e06be3130b2d7c432aaa289b51692eca3d461
aasoftwaretechnologies <aasoftwaretechnologies@gmail.com>
2025-06-20 16:11:20 +0530

    Update app.test.js

--- test/app.test.js
+++ test/app.test.js
@@ -1,11 +1,15 @@
 const request = require('supertest');
 const app = require('../src/app');
+const server = app.listen(0); // Start server on a random port
 
 describe('GET /', () => {
   it('should return Hello GitHub Actions!', async () => {
-    const res = await request(app).get('/');
+    const res = await request(server).get('/');
     expect(res.statusCode).toEqual(200);
     expect(res.text).toBe('Hello GitHub Actions!');
   });
-});
 
+  afterAll(() => {
+    server.close(); // Close the server after tests
+  });
+});
