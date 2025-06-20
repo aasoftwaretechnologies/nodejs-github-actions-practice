97caa177359ba8b0a62051216364f282b03a74e4
aasoftwaretechnologies <aasoftwaretechnologies@gmail.com>
2025-06-20 16:03:22 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,1 +1,14 @@
+const express = require('express');
+const app = express();
+const PORT = 3000;
+
+app.get('/', (req, res) => {
+  res.status(200).send('Hello GitHub Actions!');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
+
+module.exports = app; // Export for testing
 
